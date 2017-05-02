af3a1bf70e5c9b1be589d636e29186138cdd6855
Ram Iyer <ram.iyer@bydeluxe.com>
2017-05-02 02:10:56 -0500

    adding factory girl factories

---
+++ spec/factories/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :account do
+    name                  'ram iyer'
+    email                 'ramky.iyer@yahoo.com'
+    password              '1234'
+    password_confirmation '1234'
+  end
+end
