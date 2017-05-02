a7312818e37a726bf4e63b3f2287981604a15e10
Ram Iyer <ram.iyer@bydeluxe.com>
2017-05-02 02:10:38 -0500

    adding database_cleaner, factory girl to rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,20 +3,33 @@ require File.expand_path('../../config/environment', __FILE__)
 abort_with_message if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'factory_girl_rails'
 require 'simplecov'
+require 'database_cleaner'
 
 SimpleCov.start 'rails'
 
 ActiveRecord::Migration.maintain_test_schema!
 
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f  }
+
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   config.use_transactional_fixtures = true
-
   config.infer_spec_type_from_file_location!
-
   config.filter_rails_from_backtrace!
+  config.include FactoryGirl::Syntax::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
 
 Shoulda::Matchers.configure do |config|
