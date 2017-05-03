be6f59aa24e48e7bb96e75ae2ad00d8b7abee946
Ram Iyer <ram.iyer@bydeluxe.com>
2017-05-03 01:19:36 -0500

    using method called current_acccount instead of attr_reader and using controller concern to create json_response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,17 @@
 class ApplicationController < ActionController::API
   before_action :authenticate_request
-  attr_reader :current_account
+  helper_method :current_account
+
+  include Response
 
   private
 
   def authenticate_request
-    @current_account = AuthorizeApiRequest.call(request.headers).result
-    render json: error_hash, status: 401 unless @current_account
+    render json: error_hash, status: 401 unless current_account
+  end
+
+  def current_account
+    @current_account ||= AuthorizeApiRequest.call(request.headers).result
   end
 
   def error_hash
