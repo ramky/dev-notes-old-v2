c9d50c41b07389971906464d6280722d371ff4d2
Ram Iyer <ram.iyer@bydeluxe.com>
2017-05-03 01:21:21 -0500

    refactoring the controller spec for index action and using support/macros to keep it dry

--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -4,16 +4,17 @@ require 'json_web_token'
 RSpec.describe TopicsController, type: :api do
   describe 'GET /topics/' do
     context 'with correct credentials' do
-      before :each do
+      it 'returns topics' do
         ram = create(:account)
-        @token = JsonWebToken.encode({account_id: ram.id})
-      end
+        auth_header_set(ram)
+        create(:topic, account_id: ram.id)
+        create(:topic, account_id: ram.id)
+        create(:topic, account_id: ram.id)
 
-      it 'should return an ok json response' do
-        expected_result =   {status: "ok"}.to_json
-        header 'Authorization',"#{@token}"
         get '/topics/'
-        expect(last_response.body).to eq(expected_result)
+
+        expect(json).not_to be_empty
+        expect(json.size).to eq(3)
       end
     end
   end
