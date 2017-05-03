5782b34fc8a276391907f58ce0f0410662049795
Ram Iyer <ram.iyer@bydeluxe.com>
2017-05-03 01:20:31 -0500

    adding default scope and all_for_account for topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,4 +3,7 @@ class Topic < ActiveRecord::Base
   belongs_to :account
 
   validates_presence_of :name
+
+  default_scope -> { order('name asc') }
+  scope :all_for_account, (->(id) { where('account_id = ?', id) })
 end
